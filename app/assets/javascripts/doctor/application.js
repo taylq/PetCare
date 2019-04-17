5d800a43846fb8f066bca44d39b4a596839e6721
taylq <taylqmail@gmail.com>
2019-04-17 20:25:02 +0700

    doctor view shedule

--- app/assets/javascripts/doctor/application.js
+++ app/assets/javascripts/doctor/application.js
@@ -46,50 +46,50 @@ $(document).click(function () {
 
 var initialize_calendar;
 initialize_calendar = function () {
-    $('#calendar').each(function () {
-        var calendar = $(this);
-        calendar.fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month'
-            },
-            selectable: true,
-            selectHelper: true,
-            editable: true,
-            eventLimit: true,
-            events: '/doctor/events',
+  $('#calendar').each(function () {
+    var calendar = $(this);
+    calendar.fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month'
+      },
+      selectable: true,
+      selectHelper: true,
+      editable: true,
+      eventLimit: true,
+      events: '/doctor/events',
 
-            eventRender: function (event, element) {
-                element.find(".fc-title").remove();
-                element.find(".fc-time").remove();
-                var new_description =
-                    event.start.format("DD/MM/YYYY HH:mm") + ' - '
-                    + event.real_end
+      eventRender: function (event, element) {
+        element.find(".fc-title").remove();
+        element.find(".fc-time").remove();
+        var new_description =
+          event.start.format("DD/MM/YYYY HH:mm") + ' - '
+          + event.real_end
 
-                ;
-                element.append(new_description).css("color", "white");
-            },
+          ;
+        element.append(new_description).css("color", "white");
+      },
 
-            eventClick: function (event, jsEvent, view) {
-                $.getScript(event.edit_url, function () { });
-            }
-        });
-    })
+      eventClick: function (event, jsEvent, view) {
+        $.getScript(event.edit_url, function () { });
+      }
+    });
+  })
 };
 $(document).ready(function () {
-    initialize_calendar();
-    $(document).on('click', '.toggle-window', function (e) {
-        e.preventDefault();
-        var panel = $(this).parent().parent();
-        var messages_list = panel.find('.messages-list');
+  initialize_calendar();
+  $(document).on('click', '.toggle-window', function (e) {
+    e.preventDefault();
+    var panel = $(this).parent().parent();
+    var messages_list = panel.find('.messages-list');
 
-        panel.find('.panel-body').toggle();
-        panel.attr('class', 'panel panel-default');
+    panel.find('.panel-body').toggle();
+    panel.attr('class', 'panel panel-default');
 
-        if (panel.find('.panel-body').is(':visible')) {
-            var height = messages_list[0].scrollHeight;
-            messages_list.scrollTop(height);
-        }
-    });
+    if (panel.find('.panel-body').is(':visible')) {
+      var height = messages_list[0].scrollHeight;
+      messages_list.scrollTop(height);
+    }
+  });
 });
